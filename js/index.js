a5f5f01ca225e0cf6617ac67379218ac52e9b125
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-07-29 14:12:33 -0300

    Arrays

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// metodos para arrays
+
+
+let numeros = [1, 4, 10, 14, 18, 20];
+
+numeros.forEach(verificar);
+
+function printNumber(value, index, array) {
+    console.log(`Index: ${index} Valor: ${value}`)
+}
