4a7e405fa1d4a8ae3fee5cb4fa471340b71659f1
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-12-29 19:43:41 -0300

    particularities

---
+++ particularidades_js/js/index.js
@@ -0,0 +1,22 @@
+// Codigo lido de cima para baixo
+console.log("Primeiro");
+
+console.log("Segundo")
+
+// Javascript é Case-sensitive
+let nome = "Brenno";
+
+let Nome = "Jesus";
+
+console.log(nome);
+
+// Tipagem fraca, comecou com um number, e virou uma string
+var a = 1;
+
+console.log(typeof a)
+
+a = 'teste';
+
+console.log(a);
+
+console.log(typeof a)
