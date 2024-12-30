7b6c71dec591c971a6dbee812fbe9cfe005cab4c
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-12-30 10:09:54 -0300

    Number

---
+++ number/js/index.js
@@ -0,0 +1,25 @@
+var numero = 5;
+
+console.log(numero);
+console.log(typeof numero);
+
+var float = 5.53;
+
+console.log(float);
+console.log(typeof float);
+
+//isso é uma string
+var textoComNumero = "1000";
+
+console.log(typeof  textoComNumero);
+
+// valores internos do number
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
+
+// fazendo a soma de number mais number
+var x = 10;
+var y = 40;
+var soma = x + y;
+console.log(soma);
