cce3655be6a253d2276911560c46bbea10733cf2
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-12-30 15:08:31 -0300

    Array

---
+++ arrays/js/index.js
@@ -0,0 +1,27 @@
+// array de numeros
+let numeros = [10, 100, 2, 60,1];
+
+console.log(numeros);
+
+console.log(typeof numeros);
+
+console.log(numeros[0]);
+
+console.log(numeros[1]);
+
+// array de string
+let familia = ["Kaio", "Marcia", "Eduardo", "Thays", "Yasmin"];
+
+
+familia[1] = "Jesus";
+
+familia[5] = "Cristo";
+
+console.log(familia);
+
+console.log(familia[1]);
+
+//array com um objeto dentro, podemos colocar qualquer tipo
+let arr = ["Brenno", 27, "Programador", 4400, true, {nome: "Jesus", carro: "Onix"}];
+
+console.log(arr);
