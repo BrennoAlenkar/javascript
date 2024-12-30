912e6bda10b0c73e78581066ae6065f164f5719a
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-12-30 09:39:27 -0300

    Variable

---
+++ variaveis/js/index.js
@@ -0,0 +1,40 @@
+var nome = "Brenno";
+
+nome =  10;
+
+console.log(nome);
+
+var idade = 27;
+
+console.log(idade);
+
+var $valor = 200;
+var valor4 = 4;
+
+var nome_pessoa = "Camila";
+
+console.log($valor);
+
+let meuNomeCompleto = "Brenno Eduardo Alencar de Paula";
+
+console.log(meuNomeCompleto);
+
+//Variável com valor constante
+const constante = "Valor constante";
+
+console.log(constante);
+
+//Testando somar valores com variáveis
+
+var x = 10;
+
+var y = 20;
+
+var soma = x + y;
+
+console.log(soma);
+
+//usando o (DOM)
+let res = document.getElementById("teste");
+
+res.innerHTML = soma;
