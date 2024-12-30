f60663496833cefcfbd0d53b425823f1b4f599ca
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-12-30 10:35:28 -0300

    String

---
+++ string/js/index.js
@@ -0,0 +1,20 @@
+let nome = "Brenno";
+
+console.log(nome);
+console.log(typeof nome);
+
+// um numero entre aspas é uma string
+let meuCPF = "754.068.721-06";
+
+console.log(meuCPF);
+console.log(typeof meuCPF);
+
+let meuNome = "Brenno";
+let sobreNome = "Alencar";
+
+// string + string faz concatenação
+let nomeCompleto = meuNome + " " + sobreNome; 
+
+console.log(nomeCompleto);
+
+
