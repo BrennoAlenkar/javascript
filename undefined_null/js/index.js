d6c33b8d08f6724986e0349e415b18a7702f1636
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-12-30 12:03:14 -0300

    Undefined Null

---
+++ undefined_null/js/index.js
@@ -0,0 +1,18 @@
+//hoisting - içamento
+
+console.log(sobreNome);
+console.log(numero);
+
+var nome =  null; 
+var sobreNome;
+
+console.log(nome)
+console.log(typeof nome)
+
+nome = "Brenno";
+
+console.log(nome);
+
+console.log(sobreNome);
+
+var numero = 5;
