4e442d784377e3989a6126e512d01a3a1276c113
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2025-01-03 14:34:43 -0300

    Operador de comparação

---
+++ operadores_comparacao/js/index.js
@@ -0,0 +1,27 @@
+
+let idade = 12;
+
+if(idade >= 18) {
+    console.log("Pessoa maior de idade");
+} else if (idade < 14) {
+    console.log("Criança")
+}
+
+//Operador de comperação
+let nome = "Brenno";
+
+if(nome == "Brenno") {
+    console.log("Eu mesmo");
+} else {
+    console.log("Desconhecido");
+}
+
+let usuario = "Marcos";
+
+if(usuario === "Marcos") {
+
+    console.log("Nome de um apostolo");
+} else {
+    console.log("Não conheço");
+}
+
