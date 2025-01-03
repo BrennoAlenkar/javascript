069ca34cccfde4aeaea4b0e39dc985a2709d03ab
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2025-01-03 14:59:34 -0300

    Comparação

---
+++ if_else/js/index.js
@@ -0,0 +1,25 @@
+let date = new Date();
+
+let diaSemana = date.getDay();
+
+if (diaSemana === 0) {
+    console.log("Domingo");
+
+} else if (diaSemana === 1) {
+    console.log("Segunda");
+
+} else if (diaSemana === 2) {
+    console.log("Terça");
+
+} else if (diaSemana === 3) {
+    console.log("Quarta");
+
+} else if (diaSemana === 4) {
+    console.log("Quinta");
+
+} else if (diaSemana === 5) {
+    console.log("Sexta");
+    
+} else if (diaSemana === 6) {
+    console.log("Sabádo");
+}
