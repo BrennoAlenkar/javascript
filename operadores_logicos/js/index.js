adc56d7812576e8dbd972c4f1b70547bb0beeaad
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2025-01-08 10:17:03 -0300

    And

---
+++ operadores_logicos/js/index.js
@@ -0,0 +1,23 @@
+var idade  = 18;
+
+var nome = "joão";
+
+if (nome === "joão" && idade === 18) {
+    console.log("O joão pode entrar na aula")
+} else {
+    console.log("não é o joão")
+}
+
+if(1 == 1 && 3 > 2 && true) {
+    console.log("passou");
+}
+
+let caneta1 = "azul";
+
+let caneta2 = "vermelho";
+
+if(caneta1 == "azul" && caneta2 == "vermelho") {
+    console.log("Cliente saiu satisfeito com as 2 canetas!")
+} else {
+    console.log("Cliente não saiu com as canetas")
+}
