4c4378fca80c9eb3b078d188b00425312cb5caff
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2025-01-11 19:40:58 -0300

    while

---
+++ while/js/index.js
@@ -0,0 +1,16 @@
+let i = 0;
+while(i <= 10) {
+    console.log("Testando repetição :" + i)
+
+    i++;
+}
+
+let arr = ['brenno', 'kaio', 'eduardo', 'marcia'];
+let y = 0;
+
+while(y <= 3 ) {
+    console.log(arr[y]);
+
+    y++;
+
+}
