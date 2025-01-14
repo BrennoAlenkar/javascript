12a74eca5585a36f0730d97c3412bc0d30c70832
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2025-01-13 22:10:01 -0300

    Concat

---
+++ metodo_concat/js/script.js
@@ -0,0 +1,23 @@
+let numero = [1, 2, 3, 4, 5];
+
+let numerosMaiores = [6, 7, 8, 9, 10];
+
+let numeroAte15 = [11, 12, 13, 14, 15];
+
+let numerosJuntos = numero.concat(numerosMaiores, numeroAte15);
+
+
+
+console.log(numero);
+console.log(numerosJuntos);
+
+
+let familiaPai = ["Eduardo", "Kaio", "Guilherme", "joão"];
+
+let familiaMae = ["Marcia", "cida", "Luana", "Layne"];
+
+let novaFamilia = familiaPai.concat(familiaMae);
+
+console.log(novaFamilia);
+
+console.log(novaFamilia.length);
