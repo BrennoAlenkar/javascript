358f85a291ef5bb4aa018d313cd11e6c07bf8233
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2025-01-17 14:58:30 -0300

    Function

---
+++ funcoes/js/main.js
@@ -0,0 +1,20 @@
+function primeiraFuncao() {
+    console.log("Hello Word das funções");
+
+}
+
+primeiraFuncao();
+
+function dizerNome(nome) {
+    console.log("O nome é : " + nome);
+}
+
+dizerNome("Brenno");
+
+function soma(a, b) {
+    var soma = a + b
+    return soma;
+}
+
+console.log(soma(10,10));
+
