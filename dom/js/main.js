45be372da0c36c60125a2077e6e46319fdd9fb69
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2025-01-19 15:39:57 -0300

    Praticando

---
+++ dom/js/main.js
@@ -0,0 +1,11 @@
+var lista = ['Laranja', 'Maçã', 'Banana', 'Uva'];
+
+var listaUl = document.createElement('ul');
+
+var body = document.getElementsByTagName('body');
+
+console.log(body[0]);
+
+body[0].appendChild(listaUl);
+
+var litaNoBody = document.getElementsByTagName()
---
+++ teste/js/main.js
@@ -0,0 +1,34 @@
+const usuario = {
+    nome: "Brenno",
+    idade: 27,
+    prof: "Developer",
+    casado: false,
+    conjuge: null
+}
+
+const usuario2 = {
+    nome: "Manu",
+    idade: 18,
+    prof: "Developer",
+    casado: false,
+    conjuge: null
+}
+
+const meuArray = [{
+    nome: "Brenno",
+    idade: 27,
+    prof: "Developer",
+    casado: false,
+    conjuge: null
+},
+{
+    nome: "Manu",
+    idade: 18,
+    prof: "Developer",
+    casado: false,
+    conjuge: null
+}
+
+]
+
+console.log(meuArray[0].nome);
